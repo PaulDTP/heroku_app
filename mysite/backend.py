418d3916d225674c8c5dce8e37d91da5afe801d8
Paul Terrell-Perica <pterrellperica@gmail.com>
2023-03-11 22:13:11 +0000

    Everything is bugging out

--- mysite/backend.py
+++ mysite/backend.py
@@ -1,35 +1,51 @@
-#!/home/pauldtp/.virtualenvs/zeppelin/bin/python
+import requests
+import pandas as pd
+import numpy as np
+
+import websocket
+import json
+import git
+import time 
+
+
+from binance.client import Client
+import plotly.graph_objs as go
+import plotly.express as px
 
 # Binance
 # API key: sN8B8IP18Sba4xi7X5aX2TaQtxMu8zxr5o2FkPxZvZBDXwwFT7Sl9VYzeILh4bCi
 # secret key: 3BObFKszldGkDE9GjFe9YQpwPr0i0JJWVUGsU3EWR7KwDUCucDoVNl0GQwiOolkG 
 
 # Binance Spot Test Network: https://testnet.binance.vision/
-# API Key: iLqNC7LgXSOH6CDbgVAuyMIYhTwXI2FoeCDJzjLKMBbjBx1hg8i5RbTDs8WJEC3G
-# Secret key: o8Jyhbtt9pCPzDeIQcntwSjSKdQ67jemQIIh1n4XN72YMrEi3gOzfRuQZpSxec4W
+# API Key: odwF9bVsSsxjZnckgbSu3NfUgGqqJ2sow4OelwjEttIBB08r3Z5umQL0A03lp2Gd
+# Secret key: Q3bcPKvbvlVpzv5BQe3lj7EkWdRhevEp24Oi7TENce6xO0FiXUNQKDa47QTyyKcK
 
-import requests
-import plotly.express as px
-import pandas as pd
-import numpy as np
+# api eee8e2d1bfc46b546f74a0152e841ead60322a3eebe9197dc8aed462fb35c13c
+# secret 6644db47ed44d9da102b3c9134d52324769a8079c8c89dd1cee365d4f88d0889
 
-from binance.client import Client
-import plotly.graph_objs as go
-import websocket
-import json
 
 api_key='sN8B8IP18Sba4xi7X5aX2TaQtxMu8zxr5o2FkPxZvZBDXwwFT7Sl9VYzeILh4bCi'
 api_secret='3BObFKszldGkDE9GjFe9YQpwPr0i0JJWVUGsU3EWR7KwDUCucDoVNl0GQwiOolkG'
 
 client = Client(api_key, api_secret, testnet=True, tld='us')
 
-url = "wss://stream.binancefuture.com"
+# Websocket base endpoint
+wss = "wss://stream.binancefuture.com"
+# Base endpoint
+base = 'https://testnet.binancefuture.com'
+# User date endpoint
+
+
+def get_updated():
+    # Last update from git commit
+    repo = git.Repo("/home/pauldtp/z-algo").head.commit
+    return time.strftime("%a, %d %b %Y %H:%M", time.gmtime(repo.committed_date))
 
 def make_graph():
     # Make the API call
     url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
     response = requests.get(url)
-    data = response.json()
+    data = response.json() 
 
     # Store the data in a Pandas dataframe
     df = pd.DataFrame(data, index=[0])
--- mysite/zeppelin.py
+++ mysite/zeppelin.py
@@ -1,23 +1,21 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dash, html, dcc
 
-# user-made files
-import research
-
+# custom files
+import backend
 
 app = dash.Dash()
 
-crypto_graph = research.make_graph()
+crypto_graph = backend.make_graph()
+updated = backend.get_updated()
 
 app.layout = html.Div(children=[
-    html.H1(children="Graph"),
-    html.Div(children="Test web app"),
 
-    dcc.Graph(
-        id='crypto graph',
-        figure=crypto_graph
-    )
+    html.H2(children=f"Last updated: {updated} UTC"),
+    #html.Div(children="Menu"),
+    dcc.Dropdown(['Current Coin Prices', 'Our Trades', 'Our Returns'], 'Current Coin Prices'),
+    dcc.Graph(id='crypto graph', figure=crypto_graph),
+    dcc.Interval()
+    #, generate_table(data)
     ])
 
 if __name__ == '__main__':
